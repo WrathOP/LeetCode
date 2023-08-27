c12fe0093361f4558efa1668f69283eb1b534324
WrathOP <72385500+WrathOP@users.noreply.github.com>
2023-08-28 02:03:01 +0530

    Time: 42 ms (82.68%), Space: 16.3 MB (62.06%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 1 , x
+        while l<=r:
+            mid = (l+r)>>1
+            if mid*mid == x :
+                return mid
+            elif mid*mid< x:
+                l = mid+1
+            else:
+                r = mid-1
+        return l-1
+
+
