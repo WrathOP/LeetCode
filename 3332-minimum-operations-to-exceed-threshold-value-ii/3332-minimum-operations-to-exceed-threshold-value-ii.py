08a171f5ebee9257dc89cfee1ec9fd907ebde59b
Pratham Mittal <72385500+WrathOP@users.noreply.github.com>
2025-02-13 13:05:26 +0530

    Time: 207 ms (78.15%), Space: 35.5 MB (38.24%) - LeetHub

---
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        min_heap = nums 
+        res = 0
+        heapq.heapify(nums) 
+
+        while len(nums)> 1 and  nums[0] < k:
+            smol1 = heapq.heappop(nums)
+            smol2 = heapq.heappop(nums)
+
+            heapq.heappush(nums, (smol1 * 2)+ smol2)
+            res += 1
+
+        return res
+            
